9567b902d214121bef4e04077b7620b475d5429b
Roman Peters <mail@romanpeters.nl>
2019-05-16 22:22:55 +0200

    Remove make-shift config loading

--- mqtthawk/utils/configger.py
+++ mqtthawk/utils/configger.py
@@ -1,19 +1,28 @@
+import os
 import logging
 import yaml
 
 
 _LOGGER = logging.getLogger(__name__)
 
+def read_env_vars(CONFIG):
+    for i in ["MQTT_BROKER", "MQTT_PORT", "MQTT_USER", "MQTT_PASSWORD"]:
+        if os.environ.get(i):
+            group, var = i.lower().split('_')
+            CONFIG[group].update({var: os.environ[i]})
+    return CONFIG
 
 try:
     # Read configuration
     with open("config.yaml", 'r') as yaml_file:
         CONFIG = yaml.load(yaml_file)
 except FileNotFoundError:
-    _LOGGER.warn("Couldn't find config.yaml, trying config.yaml.example instead")
-    with open("config.yaml.example", 'r') as yaml_file:
-        CONFIG = yaml.load(yaml_file)
-    _LOGGER.warn("Using config.yaml.example")
+    _LOGGER.warn("Couldn't find config.yaml")
+    CONFIG = {}
+
+CONFIG = read_env_vars(CONFIG)
+
+
 
 def get_component_config(name):
     component_config = next((i for i in CONFIG['components'] if i['platform'] == name), None)
